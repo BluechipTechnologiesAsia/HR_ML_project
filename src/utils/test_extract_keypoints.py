04408907d7eec24cf61ad04a61761d60b25ec12f
Nimesh <fdsjkomsal@gmail.com>
2021-02-09 12:43:11 +0530

    added methods to calculate sport score

--- src/utils/test_extract_keypoints.py
+++ src/utils/test_extract_keypoints.py
@@ -1,4 +1,5 @@
-from extract_keypoints import cleanup_sentence, cleanup_bulletpoints
+from extract_keypoints import cleanup_sentence, cleanup_bulletpoints,\
+        calculate_score_clubs, advanced_search_clubs
 import unittest
 
 
@@ -40,21 +41,6 @@ class TestCleanUp(unittest.TestCase):
         self.assertEqual(filter_sentence_3, " ")
 
 
-class TestNormalSearch(unittest.TestCase):
-    """
-    """
-    def test_keywords(self):
-        """check if any keyword is present in the
-        given string
-        """
-        test_string_1 = """I went to AISEc"""
-
-    def test_scoring(self):
-        """check if the scoring mechanism is evaluated
-        properly
-        """
-
-
 class TestAdvancedSearch(unittest.TestCase):
     """
     """
@@ -62,12 +48,32 @@ class TestAdvancedSearch(unittest.TestCase):
         """check if any keyword is present in the
         given string
         """
-        test_string_1 = """I went to AISEc"""
+        pass
 
-    def test_scoring(self):
+    def test_scoring_clubs(self):
         """check if the scoring mechanism is evaluated
         properly
         """
+        test_string_1 = """I went to AIESEC"""
+        test_string_2 = """I went to Rotract"""
+        test_string_3 = """I go to school"""
+        test_string_4 = """LEO is my home"""
+        test_string_5 = """I was president of LEO club and IEASL"""
+
+        # getting the scores using the function
+        score_1 = calculate_score_clubs(test_string_1)
+        score_2 = calculate_score_clubs(test_string_2)
+        score_3 = calculate_score_clubs(test_string_3)
+        score_4 = calculate_score_clubs(test_string_4)
+        score_5 = calculate_score_clubs(test_string_5)
+
+        # if name is AIESEC score is 1.5
+        self.assertEqual(score_1, 1.5)
+        # if name is Rotract score is 1.5
+        self.assertEqual(score_2, 1.5)
+        self.assertEqual(score_3, 0)
+        self.assertEqual(score_4, 1.0)
+        self.assertEqual(score_5, 1.5)
 
 
 if __name__ == "__main__":
